f87a6517cd279fa45dfdfbd1dfcec200fc4cff9a
Dax Wann <daxwann@gmail.com>
2020-03-01 20:15:25 -0800

    reimport maven project

---
+++ src/main/java/dax/springframework/spring_DI/SpringDiApplication.java
@@ -0,0 +1,13 @@
+package dax.springframework.spring_DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiApplication.class, args);
+	}
+
+}
